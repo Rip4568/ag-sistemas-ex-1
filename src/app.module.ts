82922f86ae84d650cbfd28936c81b4e72c2b7058
Rip4568 <63131796+Rip4568@users.noreply.github.com>
2024-12-04 17:49:40 -0300

    projeto pronto. testes iniciais feitos. CRUD completo

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    //! eu sei que deveria usar .env, mas estou com pressa pois comecei a fazer os desafios as 17 horas, e tinha ate somente meia noite pra resolver
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5433,
       username: 'postgres',
-      password: 'postgres',
+      password: 'root',
       database: 'ag_sistema_ex_1_db',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
